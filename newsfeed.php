49436d6796d7df3305f3c70fe5c95741391e807b
JohnzExor <jimenojohnzyll@gmail.com>
2023-08-13 11:39:25 +0800

    improved the newsfeed

--- newsfeed.php
+++ newsfeed.php
@@ -6,31 +6,50 @@
         $user_id = $_SESSION['user_id'];
         $text = $_POST['post-text'];
 
-        $query = "INSERT INTO user_feed (user_id, text) VALUES ('$user_id', '$text')";
-        $result = mysqli_query($conn, $query);
+        if($text != null) {
+            $query = "INSERT INTO user_feed (user_id, text) VALUES ('$user_id', '$text')";
+            $result = mysqli_query($conn, $query);
+        }
     }
 ?>
 
 
 <div class="newsfeed">
-    <div class="create-post">
-        <form method="POST">
-            <label for="post-text">Create a post</label>
-            <input class="post-text" type="text" name="post-text" id="" placeholder="Create a post">
-            <input type="submit">
-        </form>
-    </div>
+    <div class="newsfeed-container">
+        <div class="create-post">
+            <form method="POST">
+                <label class="post-label" for="post-text">Create a post</label><br>
+                <input class="post-text" type="text" name="post-text" id="" placeholder="Create a post"><br>
+                <input class="post-btn" type="submit">
+            </form>
+        </div>
 
-    <?php 
-        echo "
-            
-        ";
-    ?>
+        <?php 
+            $query = "SELECT * FROM user_feed";
+            $result = mysqli_query($conn, $query);
 
-    <div class="feed">
-        <p>Johnzyll Jimeno</p>
-        <p>sadfdsasafdsafasds</p>
-        <p>data 12341</p>
+            if ($result) {
+                while ($row = $result->fetch_assoc()) {
+
+                    $user_post = $row['user_id'];
+
+                    $query_name = "SELECT CONCAT(firstname, ' ', lastname) as name FROM users WHERE id = $user_post";
+                    $result_name = mysqli_query($conn, $query_name);
+
+                    if ($result_name) {
+                        $row_name = $result_name->fetch_assoc();
+                        echo "
+                            <div class='feed'>
+                            <p class='user-name'>" . $row_name['name'] . "</p>
+                            <p>" . $row['text'] . "</p>
+                            <p class='date-posted'>" . $row['date_posted'] . "</p>
+                            </div>
+                        ";
+                    }
+                }
+            }
+        ?>
+
     </div>
 </div>
 
@@ -40,13 +59,45 @@
         height: 60em;
         color: white;
     }
-
+    
     .create-post {
-        padding-top: 5em;
+        background-color: white;
+        width: 50em;
+        margin: auto;
+        color: black;
+        text-align: center;
+        border-radius: 1em;
+        position: relative;
+        top: 10em;
+    }
+
+    .post-label {
+        font-weight: bold;
     }
 
     .create-post .post-text {
         padding-bottom: 3em;
+        border-radius: 1em;
+        border-color: black;
+        width: 90%;
+        padding: 1em;
+    }
+
+    .post-btn {
+        padding: 1em;
+        width: 10em;
+        margin: 1em;
+        background-color: #171616;
+        color: white;
+        font-weight: bold;
+        cursor: pointer;
+        border-radius: 1em;
+    }
+
+    .post-btn:hover {
+        color: black;
+        background-color: white;
+        transition: 0.5s;
     }
 
     .feed {
@@ -54,5 +105,18 @@
         background-color: white;
         width: 30em;
         padding: 1em;
+        margin: auto;
+        border-radius: 1em;
+        margin-top: 1em;
+        position: relative;
+        top: 10em;
+    }
+
+    .feed .user-name {
+        font-weight: bold;
+    }
+
+    .feed .date-posted {
+        color: gray;
     }
 </style>
